cc3ecb66b7460087dfb54e2d9c91655d85d2d7ba
Mikko Partio <mikko.partio@fmi.fi>
2024-01-02 14:51:46 +0200

    Change to directory where spec file is and ignore vcs files

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,7 +42,11 @@ rpmdev-setuptree
 name=$(rpmspec --parse $SPEC_FILE --query --queryformat "%{Name}" --srpm)
 version=$(rpmspec --parse $SPEC_FILE --query --queryformat "%{Version}" --srpm)
 
-tar -C /github/workspace/ --transform "s,^./,$name/," -czf /github/home/rpmbuild/SOURCES/${name}-${version}.tar.gz .
+tar -C /github/workspace/$(dirname $SPEC_FILE) \
+	--exclude-vcs --exclude-vcs-ignores \
+	--transform "s,^./,$name/," \
+	-czf /github/home/rpmbuild/SOURCES/${name}-${version}.tar.gz .
+
 ln -sf /github/home/rpmbuild/SOURCES/${name}-${version}.tar.gz /github/home/rpmbuild/SOURCES/${name}.tar.gz
 
 ls -lah /github/workspace/ /github/home/rpmbuild/SOURCES/
